2b39027885cf94f53d1df3045a59e49d792716c3
Cheng Tsai <tsai_cheng@yahoo.com>
2018-02-22 17:22:32 -0800

    Finish #anagram2 and #anagram3 methods

--- anagrams.rb
+++ anagrams.rb
@@ -18,3 +18,26 @@ def sub_strs(str)
   end
   total_subs
 end
+
+
+def anagram2?(str1, str2)
+  time = Time.now
+  str1_dup = str1.dup
+  str1_dup.chars.each do | char |
+    idx1 = str1.index(char)
+    idx2 = str2.index(char)
+    return false if idx2.nil?
+    str1 = str1[0...idx1] + str1[idx1+1..-1]
+    str2 = str2[0...idx2] + str2[idx2+1..-1]
+  end
+  p (Time.now - time) * 1000
+  return true if str1.length == 0 && str2.length == 0
+  false
+end
+
+def anagram3?(str1, str2)
+  time = Time.now
+  b = str1.chars.sort == str2.chars.sort
+  p (Time.now - time) * 1000
+  b
+end
